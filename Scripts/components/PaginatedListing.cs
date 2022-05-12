16edcf3b75ca50b776cf530f703c034fbc095e17
Mario Steele <mario@ruby-im.net>
2022-05-11 21:56:20 -0500

    Update PaginatedListing.cs
      - Added Support functions to Load Plugins and Templates that have been installed onto the computer, for easier perusal.  Need to add Update checks to Plugins / Templates.

--- Scripts/components/PaginatedListing.cs
+++ Scripts/components/PaginatedListing.cs
@@ -1,7 +1,9 @@
 using Godot;
+using Godot.Collections;
 using Godot.Sharp.Extras;
 using Uri = System.Uri;
 using File = System.IO.File;
+using System.IO.Compression;
 
 public class PaginatedListing : ScrollContainer
 {
@@ -46,6 +48,107 @@ public class PaginatedListing : ScrollContainer
         _topPageCount.UpdateConfig(0);
         _bottomPageCount.UpdateConfig(0);
         return;
+    }
+
+    public void UpdateAddons() {
+        foreach(AssetLibEntry ale in _listing.GetChildren()) {
+            ale.QueueFree();
+        }
+
+        _topPageCount.Visible = false;
+        _bottomPageCount.Visible = false;
+
+        foreach(AssetPlugin plgn in CentralStore.Plugins) {
+            AssetLibEntry ale = tAssetLibEntry.Instance<AssetLibEntry>();
+            ale.Title = plgn.Asset.Title;
+            ale.Category = plgn.Asset.Category;
+            ale.Author = plgn.Asset.Author;
+            ale.License = plgn.Asset.Cost;
+            ale.AssetId = plgn.Asset.AssetId;
+            ale.UpdateAvailable = false;
+            ale.Downloaded = true;
+            _listing.AddChild(ale);
+            if (plgn.Asset.IconUrl == null || plgn.Asset.IconUrl == "") {
+                ale.Icon = Util.LoadImage("res://Assets/Icons/default_project_icon.png");
+            } else {
+                if (plgn.Asset.IconUrl.StartsWith("res://")) {
+                    ale.Icon = Util.LoadImage(plgn.Asset.IconUrl);
+                } else {
+                    Uri uri = new Uri(plgn.Asset.IconUrl);
+                    string iconPath = $"{CentralStore.Settings.CachePath}/images/{plgn.Asset.AssetId}{uri.AbsolutePath.GetExtension()}";
+                    ale.SetMeta("iconPath", iconPath);
+                    if (!File.Exists(iconPath.GetOSDir().NormalizePath())) {
+                        // Implement Image Downloader through Download Queue
+                        ImageDownloader dld = new ImageDownloader(plgn.Asset.IconUrl, iconPath);
+                        dlq.Push(dld);
+                        ale.SetMeta("dld", dld);
+                    } else {
+                        Texture icon = Util.LoadImage(iconPath);
+                        if (icon == null)
+                            ale.Icon = Util.LoadImage("res://Assets/Icons/missing_icon.svg");
+                        else
+                            ale.Icon = icon;
+                    }
+                }
+            }
+        }
+    }
+
+    public void UpdateTemplates() {
+        foreach(AssetLibEntry ale in _listing.GetChildren()) {
+            ale.QueueFree();
+        }
+
+        _topPageCount.Visible = false;
+        _bottomPageCount.Visible = false;
+
+        foreach(AssetProject prj in CentralStore.Templates) {
+            AssetLibEntry ale = tAssetLibEntry.Instance<AssetLibEntry>();
+            ale.Title = prj.Asset.Title;
+            ale.Category = prj.Asset.Category;
+            ale.Author = prj.Asset.Author;
+            ale.License = prj.Asset.Cost;
+            ale.AssetId = prj.Asset.AssetId;
+            ale.UpdateAvailable = false;
+            ale.Downloaded = true;
+            _listing.AddChild(ale);
+            if (prj.Asset.IconUrl == null || prj.Asset.IconUrl == "") {
+                ale.Icon = Util.LoadImage("res://Assets/Icons/missing_icon.svg");
+            } else {
+                string iconPath;
+                if (prj.Asset.IconUrl.StartsWith("zip+")) {
+                    iconPath = $"{CentralStore.Settings.CachePath}/images/{prj.Asset.AssetId}{prj.Asset.IconUrl.GetExtension()}";
+                    string zipPath = prj.Asset.IconUrl.Substring("zip+res://".Length);
+                    if (!File.Exists(iconPath)) {
+                        using (ZipArchive za = ZipFile.Open(prj.Location,ZipArchiveMode.Read)) {
+                            ZipArchiveEntry zae = za.GetEntry(zipPath);
+                            byte[] buffer = zae.ReadBuffer();
+                            var fh = new Godot.File();
+                            fh.Open(iconPath,Godot.File.ModeFlags.Write);
+                            fh.StoreBuffer(buffer);
+                            fh.Close();
+                        }
+                    }
+                } else {
+                    Uri uri = new Uri(prj.Asset.IconUrl);
+                    iconPath = $"{CentralStore.Settings.CachePath}/images/{prj.Asset.AssetId}{uri.AbsolutePath.GetExtension()}";
+                    ale.SetMeta("iconPath", iconPath);
+                    if (!File.Exists(iconPath.GetOSDir().NormalizePath())) {
+                        // Implement Image Downloader through Download Queue
+                        ImageDownloader dld = new ImageDownloader(prj.Asset.IconUrl, iconPath);
+                        dlq.Push(dld);
+                        ale.SetMeta("dld", dld);
+                    }
+                }
+                if (File.Exists(iconPath.GetOSDir().NormalizePath())) {
+                    Texture icon = Util.LoadImage(iconPath);
+                    if (icon == null)
+                        ale.Icon = Util.LoadImage("res://Assets/Icons/missing_icon.svg");
+                    else
+                        ale.Icon = icon;
+                }
+            }
+        }
     }
 
     public void UpdateResults(AssetLib.QueryResult result) {
