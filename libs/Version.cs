5a99875d49f6d556ca29c443e0d41e81c978771f
Mario Steele <mario@ruby-im.net>
2022-07-17 18:28:10 -0500

    Bumped GodotSharpExtras
    Updated GodotSharpExtras to use 0.3.4

--- libs/Version.cs
+++ libs/Version.cs
@@ -1,7 +1,7 @@
-public class VERSION {
+public static class VERSION {
 	public static string GodotManager = "0.2.0";
 	public static string Channel = "dev";
-	public static string GodotSharpExtras = "0.2.3";
+	public static string GodotSharpExtras = "0.3.4";
 	public static string NewtonsoftJSON = "13.0.1";
 	public static string SystemIOCompression = "4.3.0";
 	public static string ImageSharp = "1.0.4";
