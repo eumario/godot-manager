5ee7ff882d4324b2f5b799ad3c3ec198f5b86517
Mario Steele <mario@ruby-im.net>
2022-09-26 02:19:21 -0500

    Implement First Run Wizard
    Created UI and base classes for Godot Manager First Run Wizard.

---
+++ Scripts/components/Dialogs/FirstRunWizard.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+/*
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Godot Manager
+Icon=/home/eumario/Apps/GodotManager/godot.png
+Exec="/home/eumario/Apps/GodotManager/GodotManager.x86_64" %f
+Comment=Godot Project and Engine Version Manager
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=Godot-Manager
+StartupNotify=true
+*/
+
+public class FirstRunWizard : ReferenceRect
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
