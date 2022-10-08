21c83735128de8252df6c89750c5427ad892b514
Mario Steele <mario@ruby-im.net>
2022-10-07 22:54:31 -0500

    Update Version.cs
    Bump version to 0.2.1 for Next Release of Godot Manager

--- libs/Version.cs
+++ libs/Version.cs
@@ -1,5 +1,5 @@
 public static class VERSION {
-	public static string GodotManager = "0.2.0";
+	public static string GodotManager = "0.2.1";
 	public static string Channel = "dev";
 	public static string GodotSharpExtras = "0.3.4";
 	public static string NewtonsoftJSON = "13.0.1";
