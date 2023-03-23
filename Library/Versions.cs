f619252eecdfa9fbacf55b6615b90cac899ba1ab
Mario Steele <mario@ruby-im.net>
2023-03-22 22:27:50 -0500

    Update Versions.cs
    Updated GodotVersion to use SemVersion to ensure all version information follows same format.

--- Library/Versions.cs
+++ Library/Versions.cs
@@ -1,13 +1,18 @@
 using Godot;
 using System;
 using System.Reflection;
+using Godot.Collections;
 using GodotManager.Library.Utility;
 using Semver;
 
 namespace GodotManager.Library;
 public static class Versions
 {
+    private static readonly Dictionary _godotVersion = Engine.GetVersionInfo();
     public static readonly SemVersion GodotManager = SemVersion.ParsedFrom(0,3,0,"dev");
+
+    public static readonly SemVersion GodotVersion = SemVersion.ParsedFrom(_godotVersion["major"].AsInt32(),
+        _godotVersion["minor"].AsInt32(), _godotVersion["patch"].AsInt32(), _godotVersion["status"].AsString());
     public static readonly SemVersion GodotSharp = Util.GetVersion(typeof(Godot.Variant));
     public static readonly SemVersion ImageSharp = Util.GetVersion(typeof(SixLabors.ImageSharp.Color));
     public static readonly SemVersion Octokit = Util.GetVersion(typeof(Octokit.Account));
