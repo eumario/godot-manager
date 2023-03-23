c4870fecb3f3e83323a7d9c3e170e136d7a93b99
Mario Steele <mario@ruby-im.net>
2023-03-22 22:34:38 -0500

    Update GodotPanel.cs
    Started work on Godot Panel, Implementing Fetching of Data from Github, Mirror Manager still needs to be updated, as well as implementing functions of each LineEntry.

--- Library/Components/Panels/GodotPanel.cs
+++ Library/Components/Panels/GodotPanel.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Linq;
 using Godot;
 using Godot.Sharp.Extras;
 using GodotManager.Library.Components.Controls;
 using GodotManager.Library.Components.Dialogs;
+using GodotManager.Library.Data;
 
 // namespace
 namespace GodotManager.Library.Components.Panels;
@@ -26,6 +29,9 @@ public partial class GodotPanel : Panel
 	#region Private Variables
 
 	private bool _embedded = false;
+	private Managers.Github.Godot _godot = null;
+	private Managers.Github.GodotManager _godotManager = null;
+	private BusyDialog _dialog;
 	#endregion
 	
 	#region Public Variables
@@ -49,16 +55,29 @@ public partial class GodotPanel : Panel
 	public override void _Ready()
 	{
 		this.OnReady();
+
+		_godot = new Managers.Github.Godot();
+		_godot.ReleaseCount += (count) =>
+		{
+			_dialog.BylineText = $"Processing 0 of {count} releases...";
+		};
+
+		_godot.ReleaseProgress += (current, max) =>
+		{
+			_dialog.BylineText = $"Processing {current} of {max} releases...";
+		};
 		
 		// Rest of Initialization Functions
 		Embedded = _embedded;
 		_actions.ButtonClicked += HandleActions;
+		VisibilityChanged += VisibilityChanged_GodotPanel;
+
 	}
 	#endregion
 	
 	#region Event Handlers
 
-	void HandleActions(int index)
+	private void HandleActions(int index)
 	{
 		switch (index)
 		{
@@ -76,9 +95,45 @@ public partial class GodotPanel : Panel
 				break;
 		}
 	}
+
+	private async void VisibilityChanged_GodotPanel()
+	{
+		{
+			if (!Visible) return;
+
+			if (DateTime.UtcNow - Database.Settings.LastCheck >= Database.Settings.UpdateCheckInterval)
+			{
+				_dialog = BusyDialog.FromScene();
+				_dialog.BylineText = "Fetching Release Information...";
+				GetTree().Root.AddChild(_dialog);
+				_dialog.PopupCentered(new Vector2I(352, 150));
+				await _godot.UpdateDatabase();
+				Database.Settings.LastCheck = DateTime.UtcNow;
+				Database.FlushDatabase();
+				_dialog.QueueFree();
+			}
+
+			foreach (var child in _available.ItemList.GetChildren())
+				child.QueueFree();
+
+			foreach (var version in Database.AllGithubVersions().OrderByDescending(ver => ver.Release.TagName))
+			{
+				var item = GodotLineItem.FromScene();
+				item.GithubVersion = version;
+				_available.ItemList.AddChild(item);
+			}
+
+			SortChildren();
+		};
+	}
 	#endregion
 	
 	#region Private Support Functions
+
+	private void SortChildren()
+	{
+		
+	}
 	#endregion
 	
 	#region Public Support Functions
