b5fb7d8d7b17eaaa67d961159c2c5e29e169f801
Mario Steele <mario@ruby-im.net>
2023-03-22 22:32:02 -0500

    Projects Panel Implementation.
    Implemented Loading and modifications made to Settings in the Projects Panel.  Still need to implement Scan Directories.

--- Library/Components/Panels/Settings/ProjectsPanel.cs
+++ Library/Components/Panels/Settings/ProjectsPanel.cs
@@ -1,5 +1,9 @@
 using Godot;
 using Godot.Sharp.Extras;
+using GodotManager.Library.Components.Controls;
+using GodotManager.Library.Data;
+using GodotManager.Library.Managers.UndoManager;
+using GodotManager.Library.Utility;
 
 // namespace
 
@@ -11,9 +15,17 @@ public partial class ProjectsPanel : MarginContainer
 	#endregion
 	
 	#region Node Paths
+
+	[NodePath] private LineEditTimeout _projectPath;
+	[NodePath] private Button _browseProject;
+	[NodePath] private CheckBox _exitOnLaunch;
+	[NodePath] private CheckBox _scanOnStartup;
+	[NodePath] private ItemListWithButtons _scanDirs;
 	#endregion
 	
 	#region Private Variables
+
+	private bool _setup = false;
 	#endregion
 	
 	#region Public Variables
@@ -25,13 +37,77 @@ public partial class ProjectsPanel : MarginContainer
 		this.OnReady();
 		
 		// Rest of Initialization Functions
+		LoadSettings();
+		SetupEventHandlers();
 	}
 	#endregion
 	
 	#region Event Handlers
+
+	private void SetupEventHandlers()
+	{
+		_projectPath.TextUpdated += (text) => HistoryManager.Push(new UndoItem<string>(
+				Database.Settings.ProjectPath,
+				text,
+				(newVal) => Database.Settings.ProjectPath = newVal.NormalizePath(),
+				(oldVal) =>
+				{
+					Database.Settings.ProjectPath = oldVal.NormalizePath();
+					_projectPath.Text = oldVal.NormalizePath();
+				}
+			)
+		);
+		_browseProject.Pressed += async () =>
+		{
+			var path = await UI.BrowseFolder("Default Project Path", _projectPath.Text.NormalizePath());
+			if (path == "") return;
+			_projectPath.Text = path.NormalizePath();
+			_projectPath.EmitSignal(LineEditTimeout.SignalName.TextUpdated, path.NormalizePath());
+		};
+
+		_exitOnLaunch.Toggled += (toggle) => HistoryManager.Push(new UndoItem<bool>(
+				!toggle,
+				toggle,
+				(newVal) => Database.Settings.CloseManagerOnEdit = newVal,
+				(oldVal) =>
+				{
+					_setup = true;
+					Database.Settings.CloseManagerOnEdit = oldVal;
+					_exitOnLaunch.ButtonPressed = oldVal;
+					_setup = false;
+				}
+			)
+		);
+
+		_scanOnStartup.Toggled += (toggle) => HistoryManager.Push(new UndoItem<bool>(
+				!toggle,
+				toggle,
+				(newVal) => Database.Settings.EnableAutoScan = newVal,
+				(oldVal) =>
+				{
+					_setup = true;
+					Database.Settings.EnableAutoScan = oldVal;
+					_scanOnStartup.ButtonPressed = oldVal;
+					_setup = false;
+				}
+			)
+		);
+	}
 	#endregion
 	
 	#region Private Support Functions
+
+	private void LoadSettings()
+	{
+		_setup = true;
+		_projectPath.Text = Database.Settings.ProjectPath;
+		_exitOnLaunch.ButtonPressed = Database.Settings.CloseManagerOnEdit;
+		_scanOnStartup.ButtonPressed = Database.Settings.EnableAutoScan;
+
+		foreach (var dir in Database.Settings.ScanDirs)
+			_scanDirs.AddItem(dir);
+		_setup = false;
+	}
 	#endregion
 	
 	#region Public Support Functions
