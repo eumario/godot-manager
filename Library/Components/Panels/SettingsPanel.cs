2bb96c3234ccfef8e199f6ca2080be7ac11aa17a
Mario Steele <mario@ruby-im.net>
2023-03-22 22:31:23 -0500

    SettingsPanel Updates
    Implemented handling of HistoryManager for Settings Undo / Apply/Save to ensure settings are properly applied, or undo.

--- Library/Components/Panels/SettingsPanel.cs
+++ Library/Components/Panels/SettingsPanel.cs
@@ -2,6 +2,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 using Godot.Sharp.Extras;
+using GodotManager.Library.Components.Controls;
+using GodotManager.Library.Data;
+using GodotManager.Library.Managers;
+using GodotManager.Library.Managers.UndoManager;
 
 // namespace
 
@@ -14,12 +18,18 @@ public partial class SettingsPanel : Panel
 	
 	#region Node Paths
 
+	[NodePath] private ActionButtons _undoSave;
 	[NodePath] private Button _general;
 	[NodePath] private Button _projects;
 	[NodePath] private Button _about;
 	[NodePath] private Button _contributions;
 	[NodePath] private Button _licenses;
 	[NodePath] private TabContainer _optionPanels;
+	#endregion
+	
+	#region Singleton
+
+	[Singleton] private SignalBus _signalBus;
 	#endregion
 	
 	#region Private Variables
@@ -37,12 +47,33 @@ public partial class SettingsPanel : Panel
 		{
 			button.ButtonGroup = buttonGroup;
 		}
+		_undoSave.ButtonClicked += (index) =>
+		{
+			if (index == 0)
+			{
+				// Handle Saving
+				HistoryManager.Apply();
+				Database.FlushDatabase();
+				_undoSave.Visible = false;
+			}
+			else
+			{
+				// Handle Undoing
+				HistoryManager.Undo();
+				_undoSave.Visible = false;
+			}
+		};
+
+		_undoSave.Visible = false;
+
+		_signalBus.SettingsChanged += () => _undoSave.Visible = true;
+		_signalBus.SettingsSaved += () => _undoSave.Visible = false;
+		
 		_general.Pressed += () => _optionPanels.CurrentTab = 0;
 		_projects.Pressed += () => _optionPanels.CurrentTab = 1;
 		_about.Pressed += () => _optionPanels.CurrentTab = 2;
 		_contributions.Pressed += () => _optionPanels.CurrentTab = 3;
 		_licenses.Pressed += () => _optionPanels.CurrentTab = 4;
-		GD.Print(_general.ButtonGroup.GetButtons().ToList().Count);
 	}
 	#endregion
 	
