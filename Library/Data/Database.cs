4430f55f4048dd069d7c914768f8265d3299fe2f
Mario Steele <mario@ruby-im.net>
2023-08-02 19:55:18 -0500

    Updated Database.cs
    Updated Function for Remove Version to ensure that Database is flushed out after removal.

--- Library/Data/Database.cs
+++ Library/Data/Database.cs
@@ -255,8 +255,12 @@ public class Database
     public static bool HasVersion(string tag) =>
         Instance._versions.Query().Where(gv => gv.Tag == tag).FirstOrDefault() != null;
 
-    public static bool RemoveVersion(GodotVersion version) =>
-        Instance._versions.Delete(version.Id);
+    public static bool RemoveVersion(GodotVersion version)
+    {
+        var res = Instance._versions.Delete(version.Id);
+        FlushDatabase();
+        return res;
+    }
 
     public static List<GodotVersion> AllVersions() =>
         Instance._versions.Query()
