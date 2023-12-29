42f8bd1b60a6fc7107801c588d1ae332641e567c
Mario Steele <mario@ruby-im.net>
2023-12-28 23:24:07 -0600

    Added ProjectNodeCache to ensure a central class to store three nodes and project file in a single point to find all associated notes with a Project File.

---
+++ Library/Data/UI/ProjectNodeCache.cs
@@ -0,0 +1,19 @@
+﻿using GodotManager.Library.Components.Controls;
+using GodotManager.Library.Data.POCO.Internal;
+
+namespace GodotManager.Library.Data.UI;
+
+public class ProjectNodeCache
+{
+    public ProjectFile ProjectFile { get; set; }
+    public ProjectLineItem ListView { get; set; }
+    public ProjectIconItem GridView { get; set; }
+    public ProjectLineItem CategoryView { get; set; }
+
+    public void QueueFree()
+    {
+        ListView.QueueFree();
+        GridView.QueueFree();
+        CategoryView.QueueFree();
+    }
+}
