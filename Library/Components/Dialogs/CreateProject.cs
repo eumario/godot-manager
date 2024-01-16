6784e3895dd83d659af1fffecc91402e74e49159
Mario Steele <mario@ruby-im.net>
2024-01-16 02:24:57 -0600

    Added NodePaths for controls on the Main page of New Project dialog.
    Added logic to handle Populating Godot Engines versions for 3.x and 4.x, handle switching between Rendering setups based on 3.x and 4.x.

--- Library/Components/Dialogs/CreateProject.cs
+++ Library/Components/Dialogs/CreateProject.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using Godot;
 using Godot.Sharp.Extras;
+using GodotManager.Library.Data;
 
 // namespace
 
@@ -20,6 +22,24 @@ public partial class CreateProject : AcceptDialog
 	
 	#region Node Paths
 
+	[NodePath] private LineEdit _projectName;
+	[NodePath] private Button _createFolder;
+	
+	[NodePath] private LineEdit _projectLocation;
+	[NodePath] private TextureRect _validIcon;
+	[NodePath] private Button _browseLocation;
+
+	[NodePath] private CheckBox _godot3Version;
+	[NodePath] private CheckBox _godot4Version;
+
+	[NodePath] private OptionButton _templateSelection;
+	[NodePath] private OptionButton _godotVersionSelection;
+
+	[NodePath] private HBoxContainer _godot3Rendering;
+	[NodePath("%GLES3")] private CheckBox _gles3;
+	[NodePath("%GLES2")] private CheckBox _gles2;
+
+	[NodePath] private HBoxContainer _godot4Rendering;
 	[NodePath] private CheckBox _forwardPlus;
 	[NodePath] private CheckBox _mobile;
 	[NodePath] private CheckBox _compatability;
@@ -56,6 +76,46 @@ public partial class CreateProject : AcceptDialog
 	public override void _Ready()
 	{
 		this.OnReady();
+
+		_godot3Version.Pressed += () =>
+		{
+			_godot3Rendering.Visible = _godot3Version.ButtonPressed;
+			_godot4Rendering.Visible = !_godot3Version.ButtonPressed;
+
+			var defEng = Database.Settings.DefaultEngine3;
+			var selected = 0;
+
+			_godotVersionSelection.Clear();
+			foreach (var version in Database.AllVersion3())
+			{
+				_godotVersionSelection.AddItem(version.GetHumanReadableVersion());
+				_godotVersionSelection.SetItemMetadata(_godotVersionSelection.ItemCount - 1, version.Id);
+				selected = version.Id == defEng.Id ? _godotVersionSelection.ItemCount - 1 : 0;
+			}
+
+			_godotVersionSelection.Selected = selected;
+		};
+
+		_godot4Version.Pressed += () =>
+		{
+			_godot4Rendering.Visible = _godot4Version.ButtonPressed;
+			_godot3Rendering.Visible = !_godot4Version.ButtonPressed;
+
+			var defEng = Database.Settings.DefaultEngine4;
+			var selected = 0;
+			
+			_godotVersionSelection.Clear();
+			foreach (var version in Database.AllVersion4())
+			{
+				_godotVersionSelection.AddItem(version.GetHumanReadableVersion());
+				_godotVersionSelection.SetItemMetadata(_godotVersionSelection.ItemCount - 1, version.Id);
+				selected = version.Id == defEng.Id ? _godotVersionSelection.ItemCount - 1 : 0;
+			}
+
+			_godotVersionSelection.Selected = selected;
+		};
+
+		_godot3Version.EmitSignal(CheckBox.SignalName.Pressed);
 
 		AddCancelButton("Cancel");
 		// Rest of Initialization Functions
