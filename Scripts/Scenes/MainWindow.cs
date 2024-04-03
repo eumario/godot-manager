5668548c904c9c1b0f6e13a8ad09ab5228f27deb
Mario Steele <mario@ruby-im.net>
2024-04-03 00:32:44 -0500

    Added hook for Wizard Completed for first time run.  If we have finished, we ensure that the Directory structure we expect exists, and if we are closing the Editor without finishing the Setup, we don't save the database.

--- Scripts/Scenes/MainWindow.cs
+++ Scripts/Scenes/MainWindow.cs
@@ -11,12 +11,14 @@ public class MainWindow : Control
 	[NodePath("bg/Shell/Sidebar")]
 	ColorRect _sidebar = null;
 	Array<PageButton> _buttons;
+	bool SetupFinished = false;
 	[NodePath("bg/Shell/VC/TabContainer")] TabContainer _notebook = null;
 
-	public MainWindow() {
-		if (!CentralStore.Instance.LoadDatabase()) {
+	public MainWindow()
+	{
+		if (!CentralStore.Instance.LoadDatabase())
+		{
 			CentralStore.Settings.SetupDefaultValues();
-			CentralStore.Instance.SaveDatabase();
 		}
 	}
 
@@ -25,15 +27,16 @@ public class MainWindow : Control
 	{
 		this.OnReady();
 
-		EnsureDirStructure();
-
 		_buttons = new Array<PageButton>();
-		foreach(var pb in GetTree().GetNodesInGroup("page_buttons")) {
-			if (pb is PageButton) {
+		foreach (var pb in GetTree().GetNodesInGroup("page_buttons"))
+		{
+			if (pb is PageButton)
+			{
 				_buttons.Add(pb as PageButton);
 			}
 		}
-		foreach(var pb in _buttons) {
+		foreach (var pb in _buttons)
+		{
 			int i = _buttons.IndexOf(pb);
 			if (i == _notebook.CurrentTab)
 				pb.Activate();
@@ -51,11 +54,14 @@ public class MainWindow : Control
 
 		var res = CleanupCarriageReturns();
 
-		if (CentralStore.Settings.UseSystemTitlebar) {
+		if (CentralStore.Settings.UseSystemTitlebar)
+		{
 			OS.WindowBorderless = false;
 			GetTree().Root.GetNode<Titlebar>("SceneManager/MainWindow/bg/Shell/VC/TitleBar").Visible = false;
 			GetTree().Root.GetNode<Control>("SceneManager/MainWindow/bg/Shell/VC/VisibleSpacer").Visible = true;
-		} else {
+		}
+		else
+		{
 			OS.WindowBorderless = true;
 			GetTree().Root.GetNode<Titlebar>("SceneManager/MainWindow/bg/Shell/VC/TitleBar").Visible = true;
 			GetTree().Root.GetNode<Control>("SceneManager/MainWindow/bg/Shell/VC/VisibleSpacer").Visible = false;
@@ -64,33 +70,51 @@ public class MainWindow : Control
 		if (CentralStore.Settings.FirstTimeRun)
 		{
 			AppDialogs.FirstRunWizard.ShowDialog();
+			AppDialogs.FirstRunWizard.Connect("wizard_completed", this, "OnFirstRunWizard_Completed", null, (int)ConnectFlags.Oneshot);
 			//AppDialogs.FirstTimeInstall.Visible = true;
 		}
+		else
+		{
+			SetupFinished = true;
+			EnsureDirStructure();
+		}
+	}
+
+	private void OnFirstRunWizard_Completed()
+	{
+		SetupFinished = true;
 	}
 
 	private Array<int> CleanupCarriageReturns(Node node = null)
 	{
 		Array<int> count = new Array<int>() { 0, 0 };
-		if (node == null) {
+		if (node == null)
+		{
 			node = GetTree().Root;
 		}
 
-		foreach(Node cnode in node.GetChildren()) {
-			if (cnode.GetChildCount() > 0) {
+		foreach (Node cnode in node.GetChildren())
+		{
+			if (cnode.GetChildCount() > 0)
+			{
 				var res = CleanupCarriageReturns(cnode);
 				count[0] += res[0];
 				count[1] += res[1];
 			}
-			if (cnode is Label || cnode is TextEdit || cnode is LineEdit) {
+			if (cnode is Label || cnode is TextEdit || cnode is LineEdit)
+			{
 				var data = (string)cnode.Get("text");
-				if (data.Contains("\r")) {
-					cnode.Set("text", data.Replace("\r",""));
+				if (data.Contains("\r"))
+				{
+					cnode.Set("text", data.Replace("\r", ""));
 					count[0] += 1;
 				}
 			}
-			if (cnode is RichTextLabel) {
+			if (cnode is RichTextLabel)
+			{
 				var data = (string)cnode.Get("bbcode_text");
-				if (data.Contains("\r")) {
+				if (data.Contains("\r"))
+				{
 					cnode.Set("bbcode_text", data.Replace("\r", ""));
 					count[0] += 1;
 				}
@@ -100,8 +124,9 @@ public class MainWindow : Control
 		return count;
 	}
 
-	void EnsureDirStructure() {
-		
+	void EnsureDirStructure()
+	{
+
 		if (!Directory.Exists(CentralStore.Settings.CachePath.GetOSDir()))
 			Directory.CreateDirectory(CentralStore.Settings.CachePath.GetOSDir());
 		if (!Directory.Exists(CentralStore.Settings.CachePath.Join("Godot").GetOSDir()))
@@ -110,18 +135,21 @@ public class MainWindow : Control
 			Directory.CreateDirectory(CentralStore.Settings.CachePath.Join("AssetLib").GetOSDir());
 		if (!Directory.Exists(CentralStore.Settings.CachePath.Join("images").GetOSDir()))
 			Directory.CreateDirectory(CentralStore.Settings.CachePath.Join("images").GetOSDir());
-		if (!Directory.Exists(CentralStore.Settings.CachePath.Join("images","news").GetOSDir()))
-			Directory.CreateDirectory(CentralStore.Settings.CachePath.Join("images","news").GetOSDir());
+		if (!Directory.Exists(CentralStore.Settings.CachePath.Join("images", "news").GetOSDir()))
+			Directory.CreateDirectory(CentralStore.Settings.CachePath.Join("images", "news").GetOSDir());
 		if (!Directory.Exists(CentralStore.Settings.EnginePath.GetOSDir()))
 			Directory.CreateDirectory(CentralStore.Settings.EnginePath.GetOSDir());
 	}
-	
-	void OnPageButton_Clicked(PageButton pb) {
+
+	void OnPageButton_Clicked(PageButton pb)
+	{
 		_notebook.CurrentTab = _buttons.IndexOf(pb);
 	}
 
 	[SignalHandler("tree_exiting")]
-	void OnExitingTree() {
-		CentralStore.Instance.SaveDatabase();
+	void OnExitingTree()
+	{
+		if (SetupFinished)
+			CentralStore.Instance.SaveDatabase();
 	}
 }
