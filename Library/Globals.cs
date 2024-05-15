c9cbd949abdacee88e3f18303674acffede90159
Mario Steele <mario@ruby-im.net>
2024-05-14 23:00:53 -0500

    Added functionality to Run C# code on Main Thread, without using CallDeferred().

--- Library/Globals.cs
+++ Library/Globals.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace GodotManager.Library;
@@ -6,6 +8,8 @@ public partial class Globals : Node
 {
     public Vector2I Location;
     public Vector2I Size;
+
+    private List<Action> _mainThreadActions = [];
     
     public override void _Ready()
     {
@@ -14,5 +18,26 @@ public partial class Globals : Node
             Location = DisplayServer.WindowGetPosition();
             Size = DisplayServer.WindowGetSize();
         };
+    }
+    
+    private void ProcessOnMainThread()
+    {
+        if (_mainThreadActions.Count == 0) return;
+        var finished = new List<Action>();
+        var iterate = new List<Action>(_mainThreadActions);
+        foreach (var action in iterate)
+        {
+            action.Invoke();
+            finished.Add(action);
+        }
+
+        foreach (var action in finished) _mainThreadActions.Remove(action);
+    }
+
+    public void RunOnMain(Action action) => _mainThreadActions.Add(action);
+
+    public override void _Process(double delta)
+    {
+        ProcessOnMainThread();
     }
 }
