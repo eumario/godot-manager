c0f24b50264b278567963c1e4843e2fed403885a
Mario Steele <mario@ruby-im.net>
2025-01-23 00:24:27 -0600

    Added Singleton Pattern to Globals

--- Library/Globals.cs
+++ Library/Globals.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using Godot;
+using GodotManager.Library.Components.Controls;
 
 namespace GodotManager.Library;
 
 public partial class Globals : Node
 {
+    public static Globals? Instance { get; private set; }
     public Vector2I Location;
     public Vector2I Size;
 
@@ -13,6 +15,7 @@ public partial class Globals : Node
     
     public override void _Ready()
     {
+        Instance = this;
         TreeExiting += () =>
         {
             Location = DisplayServer.WindowGetPosition();
