50334cc8ef503390d9e9e281a5c757e6ec5461c9
Mario Steele <mario@ruby-im.net>
2025-10-04 02:15:24 -0500

    Added NewsItem
    Added Data structure for NewsItem.

---
+++ Library/Models/General/NewsItem.cs
@@ -0,0 +1,12 @@
+namespace GodotManager.Library.Models.General;
+
+public class NewsItem
+{
+    public string Headline { get; set; }
+    public string AuthorName { get; set; }
+    public string Date { get; set; }
+    public string Url { get; set; }
+    public string Blerb { get; set; }
+    public string ImagePath { get; set; }
+    public string AuthorImagePath { get; set; }
+}
