014bcb96108514733db9fe7a70afbbf04c70f01d
Mario Steele <mario@ruby-im.net>
2025-10-04 02:17:38 -0500

    Updated Community Section
    Started allowing for tab pages.

--- Library/UI/Sections/Community.cs
+++ Library/UI/Sections/Community.cs
@@ -1,6 +1,38 @@
 using Godot;
 using System;
+using Godot.Collections;
 
+[GlobalClass, SceneTree(root: "Nodes")]
 public partial class Community : PanelContainer
 {
+    [Notify, Export] public partial Array<ScrollContainer> TabWindows { get; set; }
+    public override partial void _Ready();
+    
+    [GodotOverride]
+    public void OnReady()
+    {
+        NavBar.TabChanged += lindx =>
+        {
+            var indx = (int)lindx;
+            if (TabWindows[indx] != null)
+            {
+                HideSections();
+                TabWindows[indx].Show();
+            }
+        };
+        HideSections();
+        TabWindows[0].Show();
+    }
+
+    private void HideSections()
+    {
+        foreach (var node in TabWindows)
+            node?.Hide();
+    }
+
+    public void ClearNews()
+    {
+        foreach (var node in NewsItemList.GetChildren())
+            node.QueueFree();
+    }
 }
