2aeef460c9e53625565683c305356e891d9544c2
Mario Steele <mario@ruby-im.net>
2025-10-04 00:59:47 -0500

    Updated Sidebar
    Moved header back into sidebar, instead of having it separated.

--- Library/UI/Sidebar.cs
+++ Library/UI/Sidebar.cs
@@ -1,12 +1,14 @@
 using Godot;
 using System.Collections.Generic;
 using System.Linq;
+using Godot.Collections;
 
 namespace GodotManager.Library.UI;
 
 [SceneTree(root: "Nodes")]
 public partial class Sidebar : PanelContainer
 {
+    [Export] public Array<PanelContainer> ButtonSections { get; set; }
     private List<SectionButton> _buttons = [];
     [GodotOverride]
     public void OnReady()
@@ -15,6 +17,12 @@ public partial class Sidebar : PanelContainer
         foreach (var section in _buttons)
             section.ButtonPressed += () => ToggleButtons(section);
 
+        Settings.Pressed += () =>
+        {
+            // TODO: Implement Settings Dialog
+            GD.Print("Show Settings...");
+        };
+        
         DownloadSection.ButtonPressed += () =>
         {
             DownloadSection.Deselect(true);
@@ -22,12 +30,18 @@ public partial class Sidebar : PanelContainer
             GD.Print("Show Downloads...");
         };
         ProjectSection.Select();
+        foreach (var section in ButtonSections)
+            section.Visible = false;
+        ButtonSections[0].Visible = true;
     }
 
     private void ToggleButtons(SectionButton btn)
     {
         foreach(var section in _buttons.Where(sb => sb != btn))
             section.Deselect();
+        foreach (var section in ButtonSections)
+            section.Visible = false;
+        ButtonSections[_buttons.IndexOf(btn)].Visible = true;
     }
 
     public override partial void _Ready();
