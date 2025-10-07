fa4533dbd75e8c055782e7901a2ea06d2120140c
Mario Steele <mario@ruby-im.net>
2025-10-06 22:08:07 -0500

    Added News Cache
    Added cache file for news entries, to help limit the number of hits we
    make on the Godot official website.

---
+++ Library/FileIO/NewsCache.cs
@@ -0,0 +1,31 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Godot;
+using GodotManager.Library.Models.General;
+using GodotManager.Library.Util;
+
+namespace GodotManager.Library.FileIO;
+
+public class NewsCache
+{
+    public DateTime LastUpdated { get; set; }
+    public List<NewsItem> Items { get; set; }
+
+    public static NewsCache? Load()
+    {
+        if (!File.Exists(GlobalSettings.NewsCachePath))
+            return null;
+        var data = File.ReadAllText(GlobalSettings.NewsCachePath);
+        var nc = JsonSerializer.Deserialize<NewsCache>(data);
+        return nc;
+    }
+
+    public void Save()
+    {
+        var data = JsonSerializer.Serialize(this);
+        File.WriteAllText(GlobalSettings.NewsCachePath, data);
+    }
+}
