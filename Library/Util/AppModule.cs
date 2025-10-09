50ae4ff8e66750b5a23bd52655166ef54175e725
Mario Steele <mario@ruby-im.net>
2025-10-09 12:06:15 -0500

    Added AppModule
    Added code to help with reloading of assembly, EFCore still a blocker
    for this.

--- Library/Util/AppModule.cs
+++ Library/Util/AppModule.cs
@@ -1,6 +1,23 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
+using System.Text.Json;
+
 namespace GodotManager.Library.Util;
 
-public class AppModule
+internal class AppModule
 {
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()).Unloading += alc =>
+        {
+            var assembly = typeof(JsonSerializerOptions).Assembly;
+            var updateHandlerType = assembly.GetType("System.Text.Json.JsonSerializerUpdateHandler");
+            var clearHandlerMethod =
+                updateHandlerType?.GetMethod("ClearCache", BindingFlags.Static | BindingFlags.Public);
+            clearHandlerMethod?.Invoke(null, new object?[] { null });
+        };
+    }
     
 }
