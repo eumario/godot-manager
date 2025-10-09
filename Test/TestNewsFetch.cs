9d7dcc3cb31aa4a3efe754357317da6868f57f84
Mario Steele <mario@ruby-im.net>
2025-10-09 12:09:40 -0500

    Created TestNewsFetch
    Created test to Fetch news and ensure components are properly working.

---
+++ Test/TestNewsFetch.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+[Tool, GlobalClass]
+public partial class TestNewsFetch : EditorScript
+{
+    public override partial void _Run();
+    
+    [GodotOverride]
+    public void OnRun()
+    {
+        var dlg = new Window();
+        var news = TestNews.Instantiate();
+        dlg.AddChild(news);
+        dlg.CloseRequested += () =>
+        {
+            dlg.QueueFree();
+            GD.Print("Test News Fetch closed.");
+        };
+        EditorInterface.Singleton.PopupDialogCentered(dlg, new Vector2I(800,600));
+    }
+}
---
+++ Test/test_news/TestNews.cs
@@ -0,0 +1,78 @@
+using Godot;
+using System;
+using System.Linq;
+using GodotManager.Library.Network;
+using AppContext = GodotManager.Library.Database.AppContext;
+using Logger = GodotManager.Library.Util.Logger;
+
+[Tool, GlobalClass, SceneTree(root: "Nodes")]
+public partial class TestNews : Control
+{
+    private AppContext _appContext = null;
+    [OnInstantiate]
+    public void OnCreate()
+    {
+        GD.Print("Test news ready.");
+    }
+    
+    public override partial void _Ready();
+    
+    [GodotOverride]
+    public void OnReady()
+    {
+        OS.AddLogger(new Logger());
+        FetchNews.Disabled = true;
+        
+        var dbExists = FileAccess.FileExists("res://Test/test_db.sqlite3");
+        _appContext = AppContext.InitDatabase(ProjectSettings.GlobalizePath("res://Test/test_db.sqlite3"));
+        if (!dbExists)
+        {
+            GD.Print("Database doesn't exist, creating it...");
+            var authors = new GithubAuthors();
+            authors.AuthorFetched += ai =>
+            {
+                GD.Print($"Author Fetched: {ai.Name}");
+                _appContext.AuthorInfo.Add(ai);
+            };
+            authors.AuthorFetchCompleted += (_, _) =>
+            {
+                GD.Print($"Fetch and Parsing complete, saving database.");
+                _appContext.SaveChanges();
+                Callable.From(() => FetchNews.Disabled = false).CallDeferred();
+            };
+            authors.RefreshAuthors();
+        }
+        else
+        {
+            GD.Print("Database exists, enabling fetch news...");
+            FetchNews.Disabled = false;
+        }
+        
+        FetchNews.Pressed += () =>
+        {
+            foreach (var child in NewsItems.GetChildren())
+                child.QueueFree();
+
+            var news = new NewsAggregator();
+            news.NewsArticleFetched += newsArticle =>
+            {
+                var ai = _appContext.AuthorInfo.Where(x => x.Name == newsArticle.AuthorName).FirstOrDefault();
+                if (ai != null)
+                {
+                    newsArticle.AuthorImagePath = "https://godotengine.org".PathJoin(ai.AvatarUrl);
+                }
+                else
+                {
+                    ai = _appContext.AuthorInfo.Where(x => x.Name == "default").FirstOrDefault();
+                    if (ai != null)
+                        newsArticle.AuthorImagePath = "https://godotengine.org".PathJoin(ai.AvatarUrl);
+                }
+                var item = ArticleCard.Instantiate(newsArticle);
+                Callable.From(() => NewsItems.AddChild(item)).CallDeferred();
+            };
+            news.NewsFetchCompleted += (_, _) => Callable.From(() => FetchNews.Disabled = false).CallDeferred();
+            FetchNews.Disabled = true;
+            news.FetchNews();
+        };
+    }
+}
