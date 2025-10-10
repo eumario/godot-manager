319f2fb7ea2d3173862eb093c58fa945d8237b02
Mario Steele <mario@ruby-im.net>
2025-10-09 20:59:39 -0500

    Updated SplashScreen
    Updated SplashScreen to handle loading in a different method.  Before,
    we would load the MainWindow scene by using the ChangeSceneToFile()
    method, but this would cause delay as the Main Window was being
    initialized (EG: Loading Author data, Image Data, fetching news, etc,
    etc), so instead, we create the Main Window, and add it to the Root
    hidden to start, and wait for the Ready event handler to be triggered,
    before making the Main Window scene visible, and QueueFree'ing the
    Splash Screen, making it more effective for loading process.

--- SplashScreen.cs
+++ SplashScreen.cs
@@ -5,20 +5,31 @@ namespace GodotManager;
 [SceneTree(root: "Nodes")]
 public partial class SplashScreen : Control
 {
+    public override partial void _Ready();
     [GodotOverride]
     public async void OnReady()
     {
         var version = ProjectSettings.GetSetting("application/config/version", "0.1.0").AsString();
         Version.Text = $"Version v{version}";
-        await ToSignal(GetTree().CreateTimer(.5f), "timeout");
-        GetTree().ChangeSceneToFile("res://MainWindow.tscn");
         DisplayServer.WindowSetMinSize(new Vector2I(1025, 600));
         var winTitle = $"Godot Manager v{version}";
+        
         #if DEBUG
         winTitle += " (DEBUG)";
         #endif
+        
         DisplayServer.WindowSetTitle(winTitle);
+        
+        await ToSignal(GetTree().CreateTimer(.5f), "timeout");
+        var mainWin = MainWindow.Instantiate();
+        mainWin.Visible = false;
+        mainWin.Ready += () =>
+        {
+            mainWin.Visible = true;
+            QueueFree();
+        };
+        
+        GetTree().Root.AddChild(mainWin);
+        
     }
-
-    public override partial void _Ready();
 }
