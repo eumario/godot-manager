df8fd07a3299edf123b6fe7c9f6f8455659a65d3
Mario Steele <mario@ruby-im.net>
2025-10-22 16:21:43 -0500

    Update Installs section
    Added fucntion to handle button presses on Install Engine.

--- Library/UI/Sections/Installs.cs
+++ Library/UI/Sections/Installs.cs
@@ -1,6 +1,19 @@
 using Godot;
 using System;
+using GodotManager;
 
+[SceneTree(root: "Nodes")]
 public partial class Installs : PanelContainer
 {
+    public override partial void _Ready();
+    
+    [GodotOverride]
+    public void OnReady()
+    {
+        InstallEngine.Pressed += () =>
+        {
+            var dlg = InstallGodotEditor.Instantiate();
+            MainWindow.GetInstance()!.AddChild(dlg);
+        };
+    }
 }
