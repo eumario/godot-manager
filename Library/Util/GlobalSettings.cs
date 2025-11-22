bc02b3b97c234891faa4a9877bdadc490c7b6934
Mario Steele <mario@ruby-im.net>
2025-11-22 02:34:42 -0600

    Updated GlobalSettings Added Missing AssetLibrary, TemplateLibrary and Engine Path to settings. Added IsFirstRun setting to settings. Added fucntions to Load and Save settings. Added Function to ensure all directories exist from the settings, as long as it is the first run of the Manager. Added Dummy functions for Updating settings file from old version, to new version (With future versions in mind.) Added Dummy functions to migrate from 2.x release of Godot Manager to 3.x.

--- Library/Util/GlobalSettings.cs
+++ Library/Util/GlobalSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Godot;
 using GodotManager.Library.FileIO;
@@ -12,8 +13,78 @@ public static class GlobalSettings
     public static string CacheDir { get; set; } = ProjectSettings.GlobalizePath("user://cache");
     public static string NewsImagePath => Path.Join(CacheDir, "images", "news");
     public static string NewsAvatarImagePath => Path.Join(CacheDir, "images", "avatar");
+    public static string AssetLibraryPath => Path.Join(CacheDir, "assets");
+    public static string TemplateLibraryPath => Path.Join(CacheDir, "templates");
+    
+    public static string EnginePath = ProjectSettings.GlobalizePath("user://versions");
+    
     public static string NewsCachePath = Path.Join(CacheDir, "news.json");
 
     public static SemanticVersion GodotManagerVersion = new SemanticVersion(0, 3, 0, "dev");
 
+    public static bool IsFirstRun = true;
+
+    public static void LoadSettings()
+    {
+        if (File.Exists(ProjectSettings.GlobalizePath("user://central_store.json")))
+        {
+            MigrateFrom2X();
+            return;
+        }
+        
+        if (!File.Exists(ProjectSettings.GlobalizePath("user://settings.json")))
+            return;
+        
+        var dict = Godot.Json.ParseString(File.ReadAllText(ProjectSettings.GlobalizePath("user://settings.json")))
+            .AsGodotDictionary();
+        if (new SemanticVersion(dict["GodotManagerVersion"].AsString()) < GodotManagerVersion)
+        {
+            UpgradeSettings();
+        }
+        
+        ProxyHost = dict["ProxyHost"].AsString();
+        ProxyPort = dict["ProxyPort"].AsInt32();
+        UseProxy = dict["UseProxy"].AsBool();
+        CacheDir = dict["CacheDir"].AsString();
+        EnginePath = dict["EnginePath"].AsString();
+        IsFirstRun = dict["IsFirstRun"].AsBool();
+    }
+
+    public static void SaveSettings()
+    {
+        using var fh = File.Open(ProjectSettings.GlobalizePath("user://settings.json"), FileMode.Create);
+        var dict = new Godot.Collections.Dictionary();
+        dict["ProxyHost"] = ProxyHost;
+        dict["ProxyPort"] = ProxyPort;
+        dict["UseProxy"] = UseProxy;
+        dict["CacheDir"] = CacheDir;
+        dict["EnginePath"] = EnginePath;
+        dict["GodotManagerVersion"] = GodotManagerVersion.ToString();
+        dict["IsFirstRun"] = IsFirstRun;
+        fh.Write(Godot.Json.Stringify(dict).ToAsciiBuffer());
+        fh.Close();
+    }
+
+    public static void EnsureDirectories()
+    {
+        if (!IsFirstRun)
+            return;
+
+        if (Directory.Exists(CacheDir)) return;
+        Directory.CreateDirectory(NewsImagePath);
+        Directory.CreateDirectory(NewsAvatarImagePath);
+        Directory.CreateDirectory(EnginePath);
+        Directory.CreateDirectory(AssetLibraryPath);
+        Directory.CreateDirectory(TemplateLibraryPath);
+    }
+
+    private static void UpgradeSettings()
+    {
+        GD.Print("Upgrading Settings...");
+    }
+
+    public static void MigrateFrom2X()
+    {
+        GD.Print("Migrating from Godot Manager 2.x...");
+    }
 }
