732fb9777496d93f2961d8ca8aa1c6f70ba30b12
Mario Steele <mario@ruby-im.net>
2025-11-22 02:41:06 -0600

    Updated EngineVersion Removed stored TemplateDir and Settings Dir, made them into more dynamic functions. Each Variable will determine based upon how the editor is installed. Is it SelfContained?  Are we on Windows, Linux or Mac OS, and provide the proper pathing for EditorData, EditorSettings, EditorCache and EditorSettingsFile, Still need to implement editor_layout.tres

--- Library/Models/EngineVersion.cs
+++ Library/Models/EngineVersion.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using Godot;
 using GodotManager.Library.Util;
 
 namespace GodotManager.Library.Models;
@@ -9,6 +12,52 @@ public class EngineVersion
     public string InstallationPath { get; set; }
     public string ExecutablePath { get; set; }
     public string Arguments { get; set; }
-    public string TemplatesDir { get; set; }
-    public string SettingsDir { get; set; }
+
+    [NotMapped] public bool IsSelfContained => File.Exists(Path.Combine(InstallationPath, "._sc_"));
+
+    [NotMapped]
+    public string TemplatesDir => Path.Combine(EditorData, "export_templates");
+
+    [NotMapped]
+    public string EditorData => IsSelfContained
+        ? Path.Combine(InstallationPath, "editor_data") :
+#if GODOT_LINUXBSD
+        Path.Combine(OS.GetEnvironment("HOME"), ".local", "share", "godot")
+#elif GODOT_WINDOWS
+        Path.Combine(OS.GetEnvironment("APPDATA"), "Godot")
+#elif GODOT_MACOS
+        Path.Combine(OS.GetEnvironment("HOME"), "Library", "Application Support", "Godot")
+#endif
+        ;
+
+    [NotMapped]
+    public string EditorSettings => IsSelfContained
+        ? Path.Combine(InstallationPath, "editor_data") :
+#if GODOT_LINUXBSD
+        Path.Combine(OS.GetEnvironment("HOME"), ".config", "godot")    
+#elif GODOT_WINDOWS
+        Path.Combine(OS.GetEnvironment("APPDATA"), "Godot")
+#elif GODOT_MACOS
+        Path.Combine(OS.GetEnvironment("HOME"), "Library", "Application Support", "Godot")
+#endif
+        ;
+
+    [NotMapped]
+    public string EditorCache => IsSelfContained
+        ? Path.Combine(InstallationPath, "editor_data")
+        :
+#if GODOT_LINUXBSD
+        Path.Combine(OS.GetEnvironment("HOME"), ".cache", "godot")
+#elif GODOT_WINDOWS
+        Path.Combine(OS.GetEnvironment("TEMP"), "Godot")
+#elif GODOT_MACOS
+        Path.Combine(OS.GetEnvironment("HOME"), "Library", "Caches", "Godot")
+#endif
+        ;
+
+    [NotMapped]
+    public string EditorSettingsFile => Path.Combine(EditorSettings,
+        Release.IsGodot3() ? "editor_settings-3.tres" : 
+        Release.IsGodot4() && Release.Version.Version.Minor < 3 ? "editor_settings-4.tres" :
+        $"editor_settings-{Release.Version.Version.Major}.{Release.Version.Version.Minor}.tres");
 }
