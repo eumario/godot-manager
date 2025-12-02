12021ff5742cbf24ba7a69be33b96ec8b2b3f571
Mario Steele <mario@ruby-im.net>
2025-12-02 12:15:47 -0600

    Updated InstallGodotEditor
    Updated InstallGodotEditor to handle wizard logic steps for options
    chosen when preparing an install of the Godot Editor.

--- Library/UI/Dialog/InstallGodotEditor.cs
+++ Library/UI/Dialog/InstallGodotEditor.cs
@@ -1,13 +1,10 @@
 #nullable enable
 using System.Linq;
 using Godot;
-using GodotManager;
 using GodotManager.Library.Database;
 using GodotManager.Library.Managers;
 using GodotManager.Library.Models;
-using GodotManager.Library.UI;
 using GodotManager.Library.Util;
-using Octokit;
 
 namespace GodotManager.Library.UI.Dialog;
 
@@ -35,29 +32,8 @@ public partial class InstallGodotEditor : PanelContainer
         SelectVersion.Visible = true;
         CloseButton.Pressed += QueueFree;
         CloseButton2.Pressed += QueueFree;
-        BackButton.Pressed += () =>
-        {
-            InstallOptions.Visible = false;
-            SelectVersion.Visible = true;
-            _selectedRelease = null;
-            if (_dotnetFeature != null)
-            {
-                _dotnetFeature.QueueFree();
-                _dotnetFeature = null;
-            }
-
-            if (_templatesFeature != null)
-            {
-                _templatesFeature.QueueFree();
-                _templatesFeature = null;
-            }
-
-            if (_selfContained != null)
-            {
-                _selfContained.QueueFree();
-                _selfContained = null;
-            }
-        };
+        BackButton.Pressed += ResetStepOne;
+        InstallButton.Pressed += HandleInstallQueue;
 
         var lastVersion = SemanticVersion.Parse("0.0.0");
         VBoxContainer latestContainer = new VBoxContainer();
@@ -89,6 +65,70 @@ public partial class InstallGodotEditor : PanelContainer
         discard.QueueFree();
     }
 
+    private void HandleInstallQueue()
+    {
+        // Check if we have a Release
+        if (_selectedRelease == null) return;
+        
+        // Get our Tag and Options
+        var tag = _selectedRelease.Version.ToString();
+        var path = GlobalSettings.EngineCache.PathJoin(tag);
+        var csharp = _dotnetFeature?.Install.ButtonPressed ?? false;
+        var templates = _templatesFeature?.Install.ButtonPressed ?? false;
+        var sc = _selfContained?.Install.ButtonPressed ?? false;
+        
+        // Get our Output Path and File Names
+        var stdPack = path.PathJoin(_selectedRelease.StandardUrl.GetFile());
+        var csharpPack = path.PathJoin(_selectedRelease.DotnetUrl.GetFile());
+        var tmplPack = path.PathJoin(_selectedRelease.TemplateUrl.GetFile());
+        var csharpTmplPack = path.PathJoin(_selectedRelease.DotnetTemplateUrl.GetFile());
+        
+        // Queue up our Downloads
+        DownloadManager.Instance.QueueDownload(tag, _selectedRelease.StandardUrl, _selectedRelease.StandardSize,
+            stdPack);
+        if (csharp)
+            DownloadManager.Instance.QueueDownload(tag, _selectedRelease.DotnetUrl, _selectedRelease.DotnetSize,
+                csharpPack);
+        if (templates)
+            DownloadManager.Instance.QueueDownload(tag, _selectedRelease.TemplateUrl, _selectedRelease.TemplateSize,
+                tmplPack);
+        if (templates && csharp)
+            DownloadManager.Instance.QueueDownload(tag, _selectedRelease.DotnetTemplateUrl, _selectedRelease.DotnetTemplateSize,
+                csharpTmplPack);
+        
+        // Queue up our Install
+        InstallManager.Instance.QueueInstall(tag, stdPack, GlobalSettings.EnginePath.PathJoin(tag));
+        if (csharp) InstallManager.Instance.QueueInstall(tag, csharpPack, GlobalSettings.EnginePath.PathJoin(tag + "-mono"));
+        if (templates) InstallManager.Instance.QueueInstall(tag, tmplPack, DirHelper.GetEditorDataPath(tag, sc).PathJoin("export_templates"));
+        if (templates & csharp) InstallManager.Instance.QueueInstall(tag, csharpTmplPack, DirHelper.GetEditorDataPath(tag + "-mono", sc).PathJoin("export_templates"));
+        if (sc) InstallManager.Instance.QueueInstall(tag, "", GlobalSettings.EnginePath.PathJoin(tag).PathJoin("._sc_"));
+        QueueFree();
+    }
+
+    private void ResetStepOne()
+    {
+        InstallOptions.Visible = false;
+        SelectVersion.Visible = true;
+        _selectedRelease = null;
+        if (_dotnetFeature != null)
+        {
+            _dotnetFeature.QueueFree();
+            _dotnetFeature = null;
+        }
+
+        if (_templatesFeature != null)
+        {
+            _templatesFeature.QueueFree();
+            _templatesFeature = null;
+        }
+
+        if (_selfContained != null)
+        {
+            _selfContained.QueueFree();
+            _selfContained = null;
+        }
+    }
+
     private void HandleInstall(ReleaseItem item)
     {
         if (item.Release == null) return;
@@ -105,6 +145,11 @@ public partial class InstallGodotEditor : PanelContainer
             var csharp = _selectedRelease.IsGodot3() ? "Mono Support" : "Dotnet Support"; 
             _dotnetFeature = FeatureOption.Instantiate($"{csharp} Support", _selectedRelease.DotnetSize);
             _dotnetFeature.Install.Pressed += UpdateBytes;
+            _dotnetFeature.HintTooltip = $"""
+                                           Provides {csharp} C# Support for the Godot Engine.
+                                           (Currently, this is a Separate Editor built specifically for
+                                           Godot and Separate Templates).
+                                           """;
             AvailableFeatures.AddChild(_dotnetFeature);
         }
 
@@ -112,10 +157,19 @@ public partial class InstallGodotEditor : PanelContainer
         {
             _templatesFeature = FeatureOption.Instantiate("Template Support", _selectedRelease.TemplateSize);
             _templatesFeature.Install.Pressed += UpdateBytes;
+            _templatesFeature.HintTooltip = """
+                                            Provides the standard template support, for exporting Godot Projects
+                                             to their final executable format for distribution.
+                                            """;
             AvailableFeatures.AddChild(_templatesFeature);
         }
 
         _selfContained = FeatureOption.Instantiate("Self Contained Install", -1);
+        _selfContained.Install.ButtonPressed = true;
+        _selfContained.HintTooltip = """
+                                     Installs the Godot editor with a self-contained folder for editor data.
+                                     When this is not enabled, Godot will store to the normal global configuration location.
+                                     """;
         AvailableFeatures.AddChild(_selfContained);
     }
 
@@ -137,11 +191,20 @@ public partial class InstallGodotEditor : PanelContainer
                 {
                     _templatesFeature.FeatureText = $"Template Support (w/{csharp} Support)";
                     _templatesFeature.FeatureSize = templateSize + dotnetTemplateSize;
+                    _templatesFeature.HintTooltip = $"""
+                                                    Provides the standard template support with {csharp}
+                                                    support templates, for exporting Godot Projects to 
+                                                    their final executable format for distribution.
+                                                    """;
                 }
                 else
                 {
                     _templatesFeature.FeatureText = "Template Support";
                     _templatesFeature.FeatureSize = templateSize;
+                    _templatesFeature.HintTooltip = """
+                                                    Provides the standard template support, for exporting Godot Projects
+                                                     to their final executable format for distribution.
+                                                    """;
                 }
             }
         }
