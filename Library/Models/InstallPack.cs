1b7bdaab62de5737f81d247f44e64846b51318f1
Mario Steele <mario@ruby-im.net>
2025-12-02 12:12:24 -0600

    Created InstallPack
    InstallPack is a Data class, that holds a collection of files to be
    installed, as to represent an install of a singular product.

---
+++ Library/Models/InstallPack.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Godot;
+
+namespace GodotManager.Library.Models;
+
+public partial class InstallPack : RefCounted
+{
+    public string Tag = "";
+    public List<string> Sources = [];
+    public List<string> Dests = [];
+    public int CurrentStep { get; private set; }
+    public int TotalSteps => Sources.Count;
+}
