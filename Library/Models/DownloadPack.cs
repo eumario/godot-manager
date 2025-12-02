c7dc1eb42010c65efcbf5216d8e819124a9e867e
Mario Steele <mario@ruby-im.net>
2025-12-02 12:11:50 -0600

    Created DownloadPack
    DownloadPack is a data class to hold downloads based upon a tag, and
    urls to be added to represent this singular download.

---
+++ Library/Models/DownloadPack.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace GodotManager.Library.Models;
+
+public partial class DownloadPack : Godot.RefCounted
+{
+    public string Tag;
+    public List<string> Urls = [];
+    public List<long> Sizes = [];
+    public List<string> SavePath = [];
+    private int _current = 0;
+    public int CurrentStep => _current;
+    public int TotalSteps => Urls.Count;
+    
+    public string CurrentUrl => Urls[_current];
+
+    public long CurrentSize
+    {
+        get => Sizes[_current];
+        set => Sizes[_current] = value;
+    }
+    public string CurrentSavePath => SavePath[_current];
+
+    public bool CompleteCurrent()
+    {
+        _current++;
+        return _current >= Urls.Count;
+    }
+}
