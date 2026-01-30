7e31939bd5b57d2e6eec0782cf2d36a586a2ecd9
Mario Steele <mario@ruby-im.net>
2026-01-30 15:53:54 -0600

    Updated EngineVersion
    Changed from old method/structure of one version per standard/dotnet
    editor, and instead merge into 1.  Versions folder still has 2 separate
    folders for each editor version.  Implemented all Dotnet* properties, as
    well as renamed InstallationPath and ExecutablePath to be
    StandardEditor/DotnetEditor.

--- Library/Models/EngineVersion.cs
+++ Library/Models/EngineVersion.cs
@@ -9,18 +9,23 @@ public class EngineVersion
 {
     public int Id { get; set; }
     public EngineRelease Release { get; set; }
-    public string InstallationPath { get; set; }
-    public string ExecutablePath { get; set; }
+    public string StandardEditor { get; set; }
+    public string DotnetEditor { get; set; }
     public string Arguments { get; set; }
 
-    [NotMapped] public bool IsSelfContained => File.Exists(Path.Combine(InstallationPath, "._sc_"));
+    [NotMapped] public string StandardInstallPath => StandardEditor.GetBaseDir();
+    [NotMapped] public string DotnetInstallPath => DotnetEditor.GetBaseDir();
+    
+    [NotMapped] public bool IsSelfContained => File.Exists(Path.Combine(StandardInstallPath, "._sc_"));
+    [NotMapped] public bool IsDotnetSelfContained => File.Exists(Path.Combine(DotnetInstallPath, "._sc_"));
 
-    [NotMapped]
-    public string TemplatesDir => Path.Combine(EditorData, "export_templates");
+    [NotMapped] public string TemplatesDir => Path.Combine(EditorData, "export_templates");
+    [NotMapped] public string DotnetTemplatesDir => Path.Combine(DotnetEditorData, "export_templates");
 
     [NotMapped]
     public string EditorData => IsSelfContained
-        ? Path.Combine(InstallationPath, "editor_data") :
+        ? Path.Combine(StandardInstallPath, "editor_data")
+        :
 #if GODOT_LINUXBSD
         Path.Combine(OS.GetEnvironment("HOME"), ".local", "share", "godot")
 #elif GODOT_WINDOWS
@@ -28,23 +33,50 @@ public class EngineVersion
 #elif GODOT_MACOS
         Path.Combine(OS.GetEnvironment("HOME"), "Library", "Application Support", "Godot")
 #endif
-        ;
+    ;
 
     [NotMapped]
-    public string EditorSettings => IsSelfContained
-        ? Path.Combine(InstallationPath, "editor_data") :
+    public string DotnetEditorData => IsDotnetSelfContained
+        ? Path.Combine(DotnetInstallPath, "editor_data")
+        :
 #if GODOT_LINUXBSD
-        Path.Combine(OS.GetEnvironment("HOME"), ".config", "godot")    
+        Path.Combine(OS.GetEnvironment("HOME"), ".local", "share", "godot")
 #elif GODOT_WINDOWS
         Path.Combine(OS.GetEnvironment("APPDATA"), "Godot")
 #elif GODOT_MACOS
         Path.Combine(OS.GetEnvironment("HOME"), "Library", "Application Support", "Godot")
 #endif
-        ;
+    ;
+
+    [NotMapped]
+    public string EditorSettings => IsSelfContained
+        ? Path.Combine(StandardInstallPath, "editor_data")
+        :
+#if GODOT_LINUXBSD
+        Path.Combine(OS.GetEnvironment("HOME"), ".config", "godot")
+#elif GODOT_WINDOWS
+        Path.Combine(OS.GetEnvironment("APPDATA"), "Godot")
+#elif GODOT_MACOS
+        Path.Combine(OS.GetEnvironment("HOME"), "Library", "Application Support", "Godot")
+#endif
+    ;
+    
+    [NotMapped]
+    public string DotnetEditorSettings => IsDotnetSelfContained
+        ? Path.Combine(DotnetInstallPath, "editor_data")
+        :
+#if GODOT_LINUXBSD
+        Path.Combine(OS.GetEnvironment("HOME"), ".config", "godot")
+#elif GODOT_WINDOWS
+        Path.Combine(OS.GetEnvironment("APPDATA"), "Godot")
+#elif GODOT_MACOS
+        Path.Combine(OS.GetEnvironment("HOME"), "Library", "Application Support", "Godot")
+#endif
+    ;
 
     [NotMapped]
     public string EditorCache => IsSelfContained
-        ? Path.Combine(InstallationPath, "editor_data")
+        ? Path.Combine(StandardInstallPath, "editor_data")
         :
 #if GODOT_LINUXBSD
         Path.Combine(OS.GetEnvironment("HOME"), ".cache", "godot")
@@ -53,11 +85,30 @@ public class EngineVersion
 #elif GODOT_MACOS
         Path.Combine(OS.GetEnvironment("HOME"), "Library", "Caches", "Godot")
 #endif
-        ;
+    ;
+    
+    [NotMapped]
+    public string DotnetEditorCache => IsDotnetSelfContained
+        ? Path.Combine(DotnetInstallPath, "editor_data")
+        :
+#if GODOT_LINUXBSD
+        Path.Combine(OS.GetEnvironment("HOME"), ".cache", "godot")
+#elif GODOT_WINDOWS
+        Path.Combine(OS.GetEnvironment("TEMP"), "Godot")
+#elif GODOT_MACOS
+        Path.Combine(OS.GetEnvironment("HOME"), "Library", "Caches", "Godot")
+#endif
+    ;
 
     [NotMapped]
     public string EditorSettingsFile => Path.Combine(EditorSettings,
         Release.IsGodot3() ? "editor_settings-3.tres" : 
+        Release.IsGodot4() && Release.Version.Version.Minor < 3 ? "editor_settings-4.tres" :
+        $"editor_settings-{Release.Version.Version.Major}.{Release.Version.Version.Minor}.tres");
+
+    [NotMapped]
+    public string DotnetEditorSettingsFile => Path.Combine(DotnetEditorSettings,
+        Release.IsGodot3() ? "editor_settings-3.tres" :
         Release.IsGodot4() && Release.Version.Version.Minor < 3 ? "editor_settings-4.tres" :
         $"editor_settings-{Release.Version.Version.Major}.{Release.Version.Version.Minor}.tres");
 }
