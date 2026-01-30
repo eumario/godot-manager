901b1911c75a2c5e9e1cef0dbc45f1824075518d
Mario Steele <mario@ruby-im.net>
2026-01-30 16:02:18 -0600

    Updated EngineLineItem
    Added script for EngineLineItem, to handle populating the control with
    the EngineVersion for the installed version of the Editor.  Need to
    update Tags (Currently only showing Linux), to Show Platform,
    Dotnet/Mono Installed, and Templates Installed.

--- Library/UI/EngineLineItem.cs
+++ Library/UI/EngineLineItem.cs
@@ -1,6 +1,26 @@
 using Godot;
 using System;
+using GodotManager.Library.Models;
 
+[SceneTree(root: "Nodes")]
 public partial class EngineLineItem : PanelContainer
 {
+    private EngineVersion? Engine;
+    [OnInstantiate]
+    public void Initialize(EngineVersion version)
+    {
+        Engine = version;
+    }
+
+    public override partial void _Ready();
+
+    [GodotOverride]
+    public void OnReady()
+    {
+        if (Engine == null) return;
+        var vers = Engine.Release.Version;
+        MajMinVersion.Text = $"Godot {vers.Major}.{vers.Minor}";
+        FullVersion.Text = $"({vers.Major}.{vers.Minor}.{vers.Build}-{vers.SpecialVersion})";
+        Installpath.Text = Engine.StandardInstallPath;
+    }
 }
