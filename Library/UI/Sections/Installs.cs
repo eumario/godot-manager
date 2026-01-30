47e842e3a55bffb0375162ce71e7d3d4a8d794ac
Mario Steele <mario@ruby-im.net>
2026-01-30 16:00:04 -0600

    Updated Installs
    Added event handler for when an installation is completed, to refresh
    the list of installed engines.

--- Library/UI/Sections/Installs.cs
+++ Library/UI/Sections/Installs.cs
@@ -1,7 +1,9 @@
 using Godot;
 using System;
+using System.Linq;
 using GodotManager;
 using GodotManager.Library.UI.Dialog;
+using GodotManager.Library.Util;
 
 namespace GodotManager.Library.UI.Sections;
 
@@ -16,7 +18,30 @@ public partial class Installs : PanelContainer
         InstallEngine.Pressed += () =>
         {
             var dlg = InstallGodotEditor.Instantiate();
+            dlg.NewInstallCompleted += () =>
+            {
+                RefreshInstalls();
+            };
             MainWindow.GetInstance()!.AddChild(dlg);
         };
+        RefreshInstalls();
+    }
+
+    private void RefreshInstalls()
+    {
+        var context = MainWindow.GetInstance().Context;
+        InstallList.QueueFreeAllChildren();
+        ItemListHeader? lastHeader = null;
+        foreach (var engine in context.EngineVersions.OrderByDescending(e => e.Release.Version))
+        {
+            if (lastHeader == null || $"Godot {engine.Release.Version.Major}" != lastHeader.Header)
+            {
+                lastHeader = ItemListHeader.Instantiate($"Godot {engine.Release.Version.Major}");
+                InstallList.AddChild(lastHeader);
+            }
+
+            var line = EngineLineItem.Instantiate(engine);
+            InstallList.AddChild(line);
+        }
     }
 }
