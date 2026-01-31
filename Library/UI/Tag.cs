c15bd12b0e993203e60406bb68185ce1b370bca0
Mario Steele <mario@ruby-im.net>
2026-01-31 02:28:21 -0600

    Updated Tag
    Added SceneTree, and Instantiate() functions to Tag.

--- Library/UI/Tag.cs
+++ Library/UI/Tag.cs
@@ -1,6 +1,12 @@
 using Godot;
 using System;
 
+[SceneTree(root: "Nodes")]
 public partial class Tag : PanelContainer
 {
+    [OnInstantiate]
+    public void Initialize(string name)
+    {
+        TagName.Text = name;
+    }
 }
